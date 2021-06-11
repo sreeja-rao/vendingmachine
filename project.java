435bdfc0b6816ac5ca2a7c57aa003822772c2e30
Sreeja Thakkalapalli <70264044+sreeja-rao@users.noreply.github.com>
2021-06-11 19:43:23 +0530

    Create project.java

---
+++ project.java
@@ -0,0 +1,115 @@
+package project;
+
+import java.util.Scanner;
+
+public class vending {
+    static String name;
+    static int qty,price,n;
+    static int total;
+    static int i;
+    private static void dairy() {
+        System.out.println("\nIndex  Items            Price\n");
+        System.out.println("\n_____  _________        ___________\n");
+        System.out.println("\n 1.    milk             55 rs/lt\n");
+        System.out.println("\n 2.    curd             99 rs/kg\n");
+        System.out.println("\n 3.    lassi            65 rs/lt\n");
+        System.out.println("\n 4.    chees            174 rs/kg\n");
+        System.out.println("\n 5.    butter           490 rs/kg\n");
+        System.out.println("\n 6.    milk shake       30 rs/bottel\n");
+        System.out.println("\n 7.    ice cream        300 rs/lt\n");
+        System.out.println("\n 8.    cone ice cream   25 rs\n");
+        System.out.println("\n 9.    ice cream cakes  500 rs/kg\n");
+        cart();
+    }
+    private static void soaps() {
+        System.out.println("\n Index   Items    Price\n");
+        System.out.println("\n_______  ______   _______\n");
+        System.out.println("\n 1.       dove     40 Rs\n");
+        System.out.println("\n 2.       lux      25 Rs\n");
+        System.out.println("\n 3.       cintal   34 Rs\n");
+        System.out.println("\n 4.       ayush    32 Rs\n");
+        System.out.println("\n 5.       santoor  32 Rs\n");
+        System.out.println("\n 6.       rexona   32 Rs\n");
+        System.out.println("\n 7.       himalaya 40 Rs\n");
+        cart();
+    }
+    private static void snacks_hot() {
+        System.out.println("\n Index    Items          Price \n");
+        System.out.println("\n ______   _______        ________\n");
+        System.out.println("\n 1.       chips          150 Rs/kg\n");
+        System.out.println("\n 2.       bhujiya        200 Rs/kg\n");
+        System.out.println("\n 3.       mixtuer        200 Rs/kg\n");
+        System.out.println("\n 5.       chakodi        200 Rs/kg\n");
+        System.out.println("\n 6.       mixed mixtuer  300 Rs/kg\n");  
+        cart();
+    }
+    private static void snacks_sweet() {
+        System.out.println("\n Index    Items       Price \n");
+        System.out.println("\n ______   _______     _______\n");
+        System.out.println("\n 1.       biscuits    50 Rs\n");
+        System.out.println("\n 2.       cookies     80 Rs\n");
+        System.out.println("\n 3.       chocolates  150 Rs\n");
+        System.out.println("\n 4.       sweets      200 Rs/kg\n"); 
+        cart();
+    }
+    private static void drinks() {
+        System.out.println("\n Index     Items       price\n");
+        System.out.println("\n ______    ______      ________\n");
+        System.out.println("\n 1.        mango       70 Rs/lt\n");
+        System.out.println("\n 2.        orange      60 Rs/lt\n");
+        System.out.println("\n 3.        milk shake  70 Rs/lt\n");
+        System.out.println("\n 4.        pine apple  60 Rs/lt \n");
+        System.out.println("\n 5.        soft drinks 40 Rs\n");
+        cart();
+    }
+    private static void cart() {
+        int items[] = new int[30];
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter how many items you want to add to cart:");
+        n= sc.nextInt();
+        for(i=0; i<=n ; i++){
+            
+            System.out.println("add items");
+            // here i wanted to take input at particular index like in c lag we do like this index[i].name how to do in java
+            System.out.println("item name:");
+            items[i]= sc.nextInt();
+            System.out.println("quantity:");
+            qty=sc.nextInt();
+            System.out.println("price");
+            price=sc.nextInt();
+            total= qty*price;
+            System.out.println("total amount to be paid by coustome: " + total);
+            sc.close();
+        }
+      
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ch;
+        System.out.println("\nchoose in which categorie you want to shop\n");
+        System.out.println("\n 1.dairy \n 2.soaps \n 3.snacks hot \n 4.snacks sweet\n 5.drinks\n 6.exit\n");
+        do{
+        System.out.println("\nenter your choice\n");
+        ch = sc.nextInt();
+        switch (ch) {
+                case 1:dairy();
+                break;
+                case 2:soaps();
+                break;
+                case 3:snacks_hot();
+                break;
+                case 4:snacks_sweet();
+                break;
+                case 5:drinks();
+                break;
+                case 6:System.out.println("BYE!");
+                break;
+        }
+        System.out.println("do you want to add more items?");
+
+    }while(ch!=6);
+        
+        sc.close();
+    }
+}
